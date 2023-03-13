313bfc4e05ef70a8fd33a95f9ff0c71f545f8920
Tommy Hoang <89426160+toho5136@users.noreply.github.com>
2023-03-13 16:34:37 -0600

    Delete Project4 directory

--- Project4/src/Observer.java
+++
@@ -1,6 +0,0 @@
-// Used source https://www.tutorialspoint.com/design_pattern/observer_pattern.htm
-
-public abstract class Observer {
-    protected FNCD fncd;
-    public abstract void update(int days, double payed, double addedFunds);
-}
