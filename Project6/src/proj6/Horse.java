28d0fd3751839e2b74bafb030e328ec37746baf5
Tommy Hoang <toho5136@colorado.edu>
2023-04-17 11:17:58 -0600

    horse class

--- Project6/src/proj6/Horse.java
+++ Project6/src/proj6/Horse.java
@@ -91,5 +91,124 @@ public class Horse {
 		int enduranceRoll = ThreadLocalRandom.current().nextInt(1, 100 + 1);
 		int traitRoll = ThreadLocalRandom.current().nextInt(1, 5 + 1);
 		int ageRoll = ThreadLocalRandom.current().nextInt(2, 5 + 1); // Horses typically start racing at 2 and retire at 6.
+		
+		if (breedRoll == 1) {
+			setBreed("Thoroughbred"); //known for speed and athleticism, often used for horse racing
+			if (speedRoll < 60) {
+				setSpeed(speedRoll + 30);
+			}
+			else {
+				setSpeed(speedRoll);
+			}
+			if (agilityRoll < 40) {
+				setAgility(agilityRoll + 10);
+			}
+			else {
+				setAgility(agilityRoll);
+			}
+			setEndurance(enduranceRoll);
+			setAge(ageRoll);
+		}
+		else if (breedRoll == 2) {
+			setBreed("Arabian"); //high energy and endurance, used for long-distance trail riding
+			setSpeed(speedRoll);
+			if (agilityRoll < 40) {
+				setAgility(agilityRoll + 10);
+			}
+			else {
+				setAgility(agilityRoll);
+			}
+			if (enduranceRoll < 60) {
+				setEndurance(enduranceRoll + 30);
+			}
+			else {
+				setEndurance(enduranceRoll);
+			}
+			setAge(ageRoll);
+		}
+		
+		else if (breedRoll == 3) {
+			setBreed("Quarter House"); //versatility, used for rodeo events, ranch work, and trail riding
+			if (speedRoll < 40) {
+				setSpeed(speedRoll + 10);
+			}
+			else {
+				setSpeed(speedRoll);
+			}
+			if (agilityRoll < 40) {
+				setAgility(agilityRoll + 15);
+			}
+			else {
+				setAgility(agilityRoll);
+			}
+			if (enduranceRoll < 40) {
+				setEndurance(enduranceRoll + 15);
+			}
+			else {
+				setEndurance(enduranceRoll);
+			}
+			setAge(ageRoll);
+		}
+		else if (breedRoll == 4) {
+			setBreed("Andalusian"); //elegant movement and strong build. used for classical riding
+			if (speedRoll < 40) {
+				setSpeed(speedRoll + 10);
+			}
+			else {
+				setSpeed(speedRoll);
+			}
+			if (agilityRoll < 60) {
+				setAgility(agilityRoll + 30);
+			}
+			else {
+				setAgility(agilityRoll);
+			}
+			setEndurance(enduranceRoll);
+			setAge(ageRoll);
+		}
+		else {
+			setBreed("Clydesdale"); //size and strength, used for pulling wagons and plows.
+			setSpeed(speedRoll);
+			if (agilityRoll < 40) {
+				setAgility(agilityRoll + 10);
+			}
+			else {
+				setAgility(agilityRoll);
+			}
+			if (enduranceRoll < 60) {
+				setEndurance(enduranceRoll + 30);
+			}
+			else {
+				setEndurance(enduranceRoll);
+			}
+			setAge(ageRoll);
+		}
+		if (traitRoll == 1) {
+			setTrait("Feisty");
+			setSpeed(getSpeed() + 10);
+			setAgility(getAgility() - 10);
+		}
+		else if (traitRoll == 2) {
+			setTrait("Speedy");
+			setSpeed(getSpeed() + 15);
+			setEndurance(getEndurance() - 10);
+		}
+		else if (traitRoll == 3) {
+			setTrait("Strong");
+			setSpeed(getSpeed() - 10);
+			setEndurance(getEndurance() + 15);
+		}
+		else if (traitRoll == 4) {
+			setTrait("Unlucky");
+			setSpeed(getSpeed() - 5);
+			setAgility(getAgility() - 5);
+			setEndurance(getEndurance() - 5);
+		}
+		else {
+			setTrait("Perfect");
+			setSpeed(getSpeed() + 10);
+			setAgility(getAgility() + 10);
+			setEndurance(getEndurance() + 10);
+		}
 	}
 }
