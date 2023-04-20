78941f5b05801d487caaf7b5278833852b58964a
Tommy Hoang <toho5136@colorado.edu>
2023-04-20 13:57:27 -0600

    Horse Factory and updated Horse

--- Project6/src/proj6/Horse.java
+++ Project6/src/proj6/Horse.java
@@ -1,5 +1,4 @@
 package proj6;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Horse {
 	private String horseName;
@@ -84,7 +83,7 @@ public class Horse {
 	}
 	
 	class Thoroughbred extends Horse {
-		public Thoroughbred(String name, int speed, int agility, int endurance, int age) {
+		public Thoroughbred(String name, int speed, int agility, int endurance, int trait, int age) {
 			this.setHorseName(name);
 			this.setBreed("Thoroughbred");
 			if (speed < 60) {
@@ -101,10 +100,37 @@ public class Horse {
 			}
 			this.setEndurance(endurance);
 			this.setAge(age);
+			if (trait == 1) {
+				this.setTrait("Feisty");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() - 10);
+			}
+			else if (trait == 2) {
+				this.setTrait("Speedy");
+				this.setSpeed(getSpeed() + 15);
+				this.setEndurance(getEndurance() - 10);
+			}
+			else if (trait == 3) {
+				this.setTrait("Strong");
+				this.setSpeed(getSpeed() - 10);
+				this.setEndurance(getEndurance() + 15);
+			}
+			else if (trait == 4) {
+				this.setTrait("Unlucky");
+				this.setSpeed(getSpeed() - 5);
+				this.setAgility(getAgility() - 5);
+				this.setEndurance(getEndurance() - 5);
+			}
+			else {
+				this.setTrait("Perfect");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() + 10);
+				this.setEndurance(getEndurance() + 10);
+			}
 		}
 	}
 	class Arabian extends Horse {
-		public Arabian(String name, int speed, int agility, int endurance, int age) {
+		public Arabian(String name, int speed, int agility, int endurance, int trait, int age) {
 			this.setHorseName(name);
 			this.setBreed("Arabian");
 			this.setSpeed(speed);
@@ -121,10 +147,37 @@ public class Horse {
 				this.setEndurance(endurance);
 			}
 			this.setAge(age);
+			if (trait == 1) {
+				this.setTrait("Feisty");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() - 10);
+			}
+			else if (trait == 2) {
+				this.setTrait("Speedy");
+				this.setSpeed(getSpeed() + 15);
+				this.setEndurance(getEndurance() - 10);
+			}
+			else if (trait == 3) {
+				this.setTrait("Strong");
+				this.setSpeed(getSpeed() - 10);
+				this.setEndurance(getEndurance() + 15);
+			}
+			else if (trait == 4) {
+				this.setTrait("Unlucky");
+				this.setSpeed(getSpeed() - 5);
+				this.setAgility(getAgility() - 5);
+				this.setEndurance(getEndurance() - 5);
+			}
+			else {
+				this.setTrait("Perfect");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() + 10);
+				this.setEndurance(getEndurance() + 10);
+			}
 		}
 	}
 	class QuarterHouse extends Horse {
-		public QuarterHouse(String name, int speed, int agility, int endurance, int age) {
+		public QuarterHouse(String name, int speed, int agility, int endurance, int trait, int age) {
 			this.setHorseName(name);
 			this.setBreed("QuarterHouse");
 			if (speed < 40) {
@@ -146,10 +199,37 @@ public class Horse {
 				this.setEndurance(endurance);
 			}
 			this.setAge(age);
+			if (trait == 1) {
+				this.setTrait("Feisty");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() - 10);
+			}
+			else if (trait == 2) {
+				this.setTrait("Speedy");
+				this.setSpeed(getSpeed() + 15);
+				this.setEndurance(getEndurance() - 10);
+			}
+			else if (trait == 3) {
+				this.setTrait("Strong");
+				this.setSpeed(getSpeed() - 10);
+				this.setEndurance(getEndurance() + 15);
+			}
+			else if (trait == 4) {
+				this.setTrait("Unlucky");
+				this.setSpeed(getSpeed() - 5);
+				this.setAgility(getAgility() - 5);
+				this.setEndurance(getEndurance() - 5);
+			}
+			else {
+				this.setTrait("Perfect");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() + 10);
+				this.setEndurance(getEndurance() + 10);
+			}
 		}
 	}
 	class Andalusian extends Horse {
-		public Andalusian(String name, int speed, int agility, int endurance, int age) {
+		public Andalusian(String name, int speed, int agility, int endurance, int trait, int age) {
 			this.setHorseName(name);
 			this.setBreed("Andalusian");
 			if (speed < 40) {
@@ -166,10 +246,37 @@ public class Horse {
 			}
 			this.setEndurance(endurance);
 			this.setAge(age);
+			if (trait == 1) {
+				this.setTrait("Feisty");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() - 10);
+			}
+			else if (trait == 2) {
+				this.setTrait("Speedy");
+				this.setSpeed(getSpeed() + 15);
+				this.setEndurance(getEndurance() - 10);
+			}
+			else if (trait == 3) {
+				this.setTrait("Strong");
+				this.setSpeed(getSpeed() - 10);
+				this.setEndurance(getEndurance() + 15);
+			}
+			else if (trait == 4) {
+				this.setTrait("Unlucky");
+				this.setSpeed(getSpeed() - 5);
+				this.setAgility(getAgility() - 5);
+				this.setEndurance(getEndurance() - 5);
+			}
+			else {
+				this.setTrait("Perfect");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() + 10);
+				this.setEndurance(getEndurance() + 10);
+			}
 		}
 	}
 	class Clydesdale extends Horse {
-		public Clydesdale(String name, int speed, int agility, int endurance, int age) {
+		public Clydesdale(String name, int speed, int agility, int endurance, int trait, int age) {
 			this.setHorseName(name);
 			this.setBreed("Clydesdale");
 			this.setSpeed(speed);
@@ -186,134 +293,33 @@ public class Horse {
 				this.setEndurance(endurance);
 			}
 			this.setAge(age);
+			if (trait == 1) {
+				this.setTrait("Feisty");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() - 10);
+			}
+			else if (trait == 2) {
+				this.setTrait("Speedy");
+				this.setSpeed(getSpeed() + 15);
+				this.setEndurance(getEndurance() - 10);
+			}
+			else if (trait == 3) {
+				this.setTrait("Strong");
+				this.setSpeed(getSpeed() - 10);
+				this.setEndurance(getEndurance() + 15);
+			}
+			else if (trait == 4) {
+				this.setTrait("Unlucky");
+				this.setSpeed(getSpeed() - 5);
+				this.setAgility(getAgility() - 5);
+				this.setEndurance(getEndurance() - 5);
+			}
+			else {
+				this.setTrait("Perfect");
+				this.setSpeed(getSpeed() + 10);
+				this.setAgility(getAgility() + 10);
+				this.setEndurance(getEndurance() + 10);
+			}
 		}
 	}
-//	public Horse(String nam){
-//		setHorseName(nam);
-//		int breedRoll = ThreadLocalRandom.current().nextInt(1, 5 + 1);
-//		int speedRoll = ThreadLocalRandom.current().nextInt(1, 100 + 1);
-//		int agilityRoll = ThreadLocalRandom.current().nextInt(1, 100 + 1);
-//		int enduranceRoll = ThreadLocalRandom.current().nextInt(1, 100 + 1);
-//		int traitRoll = ThreadLocalRandom.current().nextInt(1, 5 + 1);
-//		int ageRoll = ThreadLocalRandom.current().nextInt(2, 5 + 1); // Horses typically start racing at 2 and retire at 6.
-//		
-//		if (breedRoll == 1) {
-//			setBreed("Thoroughbred"); //known for speed and athleticism, often used for horse racing
-//			if (speedRoll < 60) {
-//				setSpeed(speedRoll + 30);
-//			}
-//			else {
-//				setSpeed(speedRoll);
-//			}
-//			if (agilityRoll < 40) {
-//				setAgility(agilityRoll + 10);
-//			}
-//			else {
-//				setAgility(agilityRoll);
-//			}
-//			setEndurance(enduranceRoll);
-//			setAge(ageRoll);
-//		}
-//		else if (breedRoll == 2) {
-//			setBreed("Arabian"); //high energy and endurance, used for long-distance trail riding
-//			setSpeed(speedRoll);
-//			if (agilityRoll < 40) {
-//				setAgility(agilityRoll + 10);
-//			}
-//			else {
-//				setAgility(agilityRoll);
-//			}
-//			if (enduranceRoll < 60) {
-//				setEndurance(enduranceRoll + 30);
-//			}
-//			else {
-//				setEndurance(enduranceRoll);
-//			}
-//			setAge(ageRoll);
-//		}
-//		
-//		else if (breedRoll == 3) {
-//			setBreed("Quarter House"); //versatility, used for rodeo events, ranch work, and trail riding
-//			if (speedRoll < 40) {
-//				setSpeed(speedRoll + 10);
-//			}
-//			else {
-//				setSpeed(speedRoll);
-//			}
-//			if (agilityRoll < 40) {
-//				setAgility(agilityRoll + 15);
-//			}
-//			else {
-//				setAgility(agilityRoll);
-//			}
-//			if (enduranceRoll < 40) {
-//				setEndurance(enduranceRoll + 15);
-//			}
-//			else {
-//				setEndurance(enduranceRoll);
-//			}
-//			setAge(ageRoll);
-//		}
-//		else if (breedRoll == 4) {
-//			setBreed("Andalusian"); //elegant movement and strong build. used for classical riding
-//			if (speedRoll < 40) {
-//				setSpeed(speedRoll + 10);
-//			}
-//			else {
-//				setSpeed(speedRoll);
-//			}
-//			if (agilityRoll < 60) {
-//				setAgility(agilityRoll + 30);
-//			}
-//			else {
-//				setAgility(agilityRoll);
-//			}
-//			setEndurance(enduranceRoll);
-//			setAge(ageRoll);
-//		}
-//		else {
-//			setBreed("Clydesdale"); //size and strength, used for pulling wagons and plows.
-//			setSpeed(speedRoll);
-//			if (agilityRoll < 40) {
-//				setAgility(agilityRoll + 10);
-//			}
-//			else {
-//				setAgility(agilityRoll);
-//			}
-//			if (enduranceRoll < 60) {
-//				setEndurance(enduranceRoll + 30);
-//			}
-//			else {
-//				setEndurance(enduranceRoll);
-//			}
-//			setAge(ageRoll);
-//		}
-//		if (traitRoll == 1) {
-//			setTrait("Feisty");
-//			setSpeed(getSpeed() + 10);
-//			setAgility(getAgility() - 10);
-//		}
-//		else if (traitRoll == 2) {
-//			setTrait("Speedy");
-//			setSpeed(getSpeed() + 15);
-//			setEndurance(getEndurance() - 10);
-//		}
-//		else if (traitRoll == 3) {
-//			setTrait("Strong");
-//			setSpeed(getSpeed() - 10);
-//			setEndurance(getEndurance() + 15);
-//		}
-//		else if (traitRoll == 4) {
-//			setTrait("Unlucky");
-//			setSpeed(getSpeed() - 5);
-//			setAgility(getAgility() - 5);
-//			setEndurance(getEndurance() - 5);
-//		}
-//		else {
-//			setTrait("Perfect");
-//			setSpeed(getSpeed() + 10);
-//			setAgility(getAgility() + 10);
-//			setEndurance(getEndurance() + 10);
-//		}
-//	}
 }
--- Project6/src/proj6/HorseFactory.java
+++ Project6/src/proj6/HorseFactory.java
@@ -1,12 +1,6 @@
 package proj6;
 import java.util.concurrent.ThreadLocalRandom;
 
-import proj6.Horse.Andalusian;
-import proj6.Horse.Arabian;
-import proj6.Horse.Clydesdale;
-import proj6.Horse.QuarterHouse;
-import proj6.Horse.Thoroughbred;
-
 public class HorseFactory {
 	public static Horse createHorse(String name) {
 		int breedRoll = ThreadLocalRandom.current().nextInt(1, 5 + 1);
@@ -16,17 +10,18 @@ public class HorseFactory {
 		int traitRoll = ThreadLocalRandom.current().nextInt(1, 5 + 1);
 		int ageRoll = ThreadLocalRandom.current().nextInt(2, 5 + 1);
 		
+		Horse horse = new Horse();
 		switch (breedRoll) {
 		case 1:
-			return new Thoroughbred(name, speedRoll, agilityRoll, enduranceRoll, ageRoll);
+			return horse.new Thoroughbred(name, speedRoll, agilityRoll, enduranceRoll, traitRoll, ageRoll);
 		case 2:
-			return new Arabian(name, speedRoll, agilityRoll, enduranceRoll, ageRoll);
+			return horse.new Arabian(name, speedRoll, agilityRoll, enduranceRoll, traitRoll, ageRoll);
 		case 3:
-			return new QuarterHouse(name, speedRoll, agilityRoll, enduranceRoll, ageRoll);
+			return horse.new QuarterHouse(name, speedRoll, agilityRoll, enduranceRoll, traitRoll, ageRoll);
 		case 4:
-			return new Andalusian(name, speedRoll, agilityRoll, enduranceRoll, ageRoll);
+			return horse.new Andalusian(name, speedRoll, agilityRoll, enduranceRoll, traitRoll, ageRoll);
 		default:
-			return new Clydesdale(name, speedRoll, agilityRoll, enduranceRoll, ageRoll);
+			return horse.new Clydesdale(name, speedRoll, agilityRoll, enduranceRoll, traitRoll, ageRoll);
 		}
 	}
 }
