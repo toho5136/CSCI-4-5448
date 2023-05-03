22c419e0047d666bdf311b2f8be5b6a3dc8d08cd
Brehn Heil <brehnheil@gmail.com>
2023-05-03 13:50:28 -0600

    Finished TrackS singleton class

--- Project6/src/proj6/TrackS.java
+++ Project6/src/proj6/TrackS.java
@@ -4,7 +4,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class TrackS {
     private int distance;
     private String trackShape;
-    private String features;
+    private boolean obstacles;
+    private int quality;
 
     public int getDistance() {
         return this.distance;
@@ -22,18 +23,60 @@ public class TrackS {
         trackShape = shape; 
     }
 
-    public String getFeatures() {
-        return this.features;
+    public boolean getObstacles() {
+        return this.obstacles;
     }
 
-    public void setFeatures(String feature) {
-        features = feature;
+    public void setObstacles(boolean obstacle) {
+        obstacles = obstacle;
     }
 
-    public TrackS(int dist, String shape, String feature) {
-        setDistance(dist);
-        setShape(shape);
-        setFeatures(feature);
+    public int getQuality() {
+        return this.quality;
     }
 
+    public void setQuality(int qualit) {
+        quality = qualit;
+    }
+
+    private static TrackS instance;
+
+    private TrackS() {
+        int trackRoll = ThreadLocalRandom.current().nextInt(0, 2);
+        int obstacleRoll = ThreadLocalRandom.current().nextInt(0, 2);
+
+        // Straight track generated
+        if (trackRoll == 0) {
+            int distRoll = ThreadLocalRandom.current().nextInt(100, 501);
+            setShape("Straight");
+            setDistance(distRoll);
+        }
+        // Curve track generated
+        else {
+            int distRoll = ThreadLocalRandom.current().nextInt(50, 251);
+            setShape("Curve");
+            setDistance(distRoll);
+        }
+
+        // False obstacle roll
+        if (obstacleRoll == 0) {
+            setObstacles(false);
+        }
+        // True obstacle roll
+        else {
+            setObstacles(true);
+        }
+
+        // Hard coded to 10 for now...
+        setQuality(10);
+    }
+
+    // Get instance for singleton pattern
+    public static TrackS getInstance() {
+        if (instance == null) {
+            instance = new TrackS();
+        }
+
+        return instance;
+    }
 }
