40651a7038bfe973cb5875770e5950fafeb0d1ff
Tommy Hoang <toho5136@colorado.edu>
2023-05-07 11:37:36 -0600

    horseracing

--- Project7/src/proj7/HorseRacing.java
+++ Project7/src/proj7/HorseRacing.java
@@ -1,5 +1,27 @@
 package proj7;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HorseRacing {
+    private ArrayList<Horse> horses = new ArrayList<>(null);
+    
+    // Incomplete
+    public void trackChoice() {
+        System.out.println("Press 1 to create custom track");
+        System.out.println("Press 2 to create random track");
 
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
+        if (input == "1") {
+
+        }
+        else if (input == "2") {
+
+        }
+        else {
+            System.out.println("Invalid input");
+        }
+    }
 }
