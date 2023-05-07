12e26fb4ffc90e47a543fed098162ab5363f7164
Tommy Hoang <toho5136@colorado.edu>
2023-05-07 11:39:50 -0600

    trackS

--- Project7/src/proj7/TrackS.java
+++ Project7/src/proj7/TrackS.java
@@ -1,5 +1,76 @@
 package proj7;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TrackS {
+    private int distance;
+    private String trackShape;
+    private boolean obstacles;
+    private int quality;
 
+    public int getDistance() {
+        return this.distance;
+    }
+
+    public void setDistance(int dist) {
+        distance = dist;
+    }
+
+    public String getShape() {
+        return this.trackShape;
+    }
+
+    public void setShape(String shape) {
+        trackShape = shape; 
+    }
+
+    public boolean getObstacles() {
+        return this.obstacles;
+    }
+
+    public void setObstacles(boolean obstacle) {
+        obstacles = obstacle;
+    }
+
+    public int getQuality() {
+        return this.quality;
+    }
+
+    public void setQuality(int qualit) {
+        quality = qualit;
+    }
+
+    public TrackS(boolean isFinish) {
+        int trackRoll = ThreadLocalRandom.current().nextInt(0, 2);
+        int obstacleRoll = ThreadLocalRandom.current().nextInt(0, 2);
+
+        // Straight track generated
+        if (trackRoll == 0 || isFinish == true) {
+            int distRoll = ThreadLocalRandom.current().nextInt(2000, 6001);
+            setShape("Straight");
+            if (isFinish == true) {
+                setDistance(0);
+            }
+            else {
+                setDistance(distRoll);
+            }
+        }
+        // Curve track generated
+        else {
+            int distRoll = ThreadLocalRandom.current().nextInt(1000, 3501);
+            setShape("Curve");
+            setDistance(distRoll);
+        }
+
+        // False obstacle roll
+        if (obstacleRoll == 0) {
+            setObstacles(false);
+        }
+        // True obstacle roll
+        else {
+            setObstacles(true);
+        }
+
+        // Hard coded to 10 for now...
+        setQuality(10);
+    }
 }
