4265814f86c1571f7cada353814a1aed02abcaf4
Tommy Hoang <toho5136@colorado.edu>
2023-05-07 11:40:20 -0600

    track

--- Project7/src/proj7/Track.java
+++ Project7/src/proj7/Track.java
@@ -1,5 +1,32 @@
 package proj7;
+import java.util.*;
 
 public class Track {
+    private ArrayList<TrackS> sections = new ArrayList<>();
+    private static Track instance;
 
+    public void addSection(TrackS section) {
+        this.sections.add(section);
+    }
+
+    public TrackS getSection(int idx) {
+        return this.sections.get(idx);
+    }
+
+    private Track(int numOfSections) {
+        for (int i = 0; i < numOfSections; i++) {
+            TrackS section = new TrackS(false);
+            addSection(section);
+        }
+        TrackS finish = new TrackS(true);
+        addSection(finish);
+    }
+
+    public static Track getInstance(int numOfSections) {
+        if (instance == null){
+            instance = new Track(numOfSections);
+        }
+
+        return instance;
+    }
 }
